5c1624053ace530708a02415fa270ee8a1d8d91b
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-12-17 11:49:27 -0800

    added 6 projects total

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,6 +4,9 @@ import Project from '../Project/Project';
 import activelyDistanced from '../../assets/images/actively-distanced.png';
 import reactTodoApp from '../../assets/images/react-todo-app.png';
 import cityExplorer from '../../assets/images/city-explorer.png';
+import weatherDashboard from '../../assets/images/weather-dashboard.png';
+import noteTaker from '../../assets/images/note-taker.png';
+import budgetTracker from '../../assets/images/budget-tracker.png';
 
 function Portfolio(props) {
 	return (
@@ -23,6 +26,21 @@ function Portfolio(props) {
 				githubLink='https://github.com/reynolkb/actively-distanced'
 				deployedLink='https://actively-distanced.herokuapp.com/'
 				projectImage={activelyDistanced}></Project>
+			<Project
+				name='Budget Tracker'
+				githubLink='https://github.com/reynolkb/budget-tracker'
+				deployedLink='https://budget-tracker-kyle.herokuapp.com/'
+				projectImage={budgetTracker}></Project>
+			<Project
+				name='Weather Dashboard'
+				githubLink='https://github.com/reynolkb/weather-dashboard'
+				deployedLink='https://reynolkb.github.io/weather-dashboard/'
+				projectImage={weatherDashboard}></Project>
+			<Project
+				name='Note Taker'
+				githubLink='https://github.com/reynolkb/note-taker'
+				deployedLink='https://note-taker-kyle.herokuapp.com/'
+				projectImage={noteTaker}></Project>
 		</div>
 	);
 }
